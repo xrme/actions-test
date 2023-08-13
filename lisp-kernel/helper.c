66e9eaf37b7cb653025a8ae32ae59e3beeb871ee
R. Matthew Emerson <rme@acm.org>
2023-08-12 20:14:14 -0700

    Add win32 directory

--- lisp-kernel/helper.c
+++ lisp-kernel/helper.c
@@ -5,6 +5,10 @@ helper(void)
     return 42;
 #elif defined(DARWIN)
     return 10;
+#elif defined(WINDOWS) && defined(WIN_64)
+    return 964;
+#elif defined(WINDOWS) && defined(WIN_32)
+    return 932;
 #else
     return 0;
 #endif
